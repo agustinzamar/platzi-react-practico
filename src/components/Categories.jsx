478d71d129bcbbd4d13cee8230ae9abd8043226d
Agustin Zamar <agustinzamar33@gmail.com>
2020-05-29 14:50:02 -0300

    curso finalizado

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "../assets/styles/components/Categories.scss";
 
 const Categories = ({ children, title }) => (
-  <div classNameName="categories">
+  <div className="categories">
     <h3 className="categories__title">{title}</h3>
     {children}
   </div>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,48 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import "../assets/styles/App.scss";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Footer from "../components/Footer";
-
 import Categories from "../components/Categories";
 import Carrousel from "../components/Carrousel";
 import CarrouselItem from "../components/CarrouselItem";
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Search />
+import "../assets/styles/App.scss";
 
-    <Categories title="Mi lista">
-      <Carrousel>
-        <CarrouselItem />
-        <CarrouselItem />
-        <CarrouselItem />
-        <CarrouselItem />
-        <CarrouselItem />
-        <CarrouselItem />
-      </Carrousel>
-    </Categories>
+import useInitialState from "../hooks/useInitialState";
 
-    <Categories title="Tendencias">
-      <Carrousel>
-        <CarrouselItem />
-        <CarrouselItem />
-        <CarrouselItem />
-        <CarrouselItem />
-      </Carrousel>
-    </Categories>
+const API = "http://localhost:3000/initalState";
 
-    <Categories title="Originales de Platzi Video">
-      <Carrousel>
-        <CarrouselItem />
-        <CarrouselItem />
-      </Carrousel>
-    </Categories>
+const App = () => {
+  const initialState = useInitialState({ API });
 
-    <Footer />
-  </div>
-);
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+
+      {initialState.myList?.length > 0 && (
+        <Categories title="Mi lista">
+          <Carrousel>
+            {initalState.myList.map((item) => (
+              <CarrouselItem key={item.id} {...item} />
+            ))}
+          </Carrousel>
+        </Categories>
+      )}
+
+      <Categories title="Tendencias">
+        <Carrousel>
+          {initialState.trends?.map((item) => {
+            return <CarrouselItem key={item.id} {...item} />;
+          })}
+        </Carrousel>
+      </Categories>
+
+      <Categories title="Originales de Platzi Video">
+        <Carrousel>
+          {initialState.originals?.map((item) => {
+            return <CarrouselItem key={item.id} {...item} />;
+          })}
+        </Carrousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
