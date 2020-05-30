4764b6be63cd700f38b14bb47f70bc6a0f0051cd
Agustin Zamar <agustinzamar33@gmail.com>
2020-05-30 14:17:17 -0300

    added: login, register and 404 components and routes

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => (
+    <>
+        <h1>No encontrado</h1>
+        <h2>
+            Vuelve al <a href="/">Home</a>
+        </h2>
+    </>
+)
+
+export default NotFound
